7397402347cbe3d91313f567486f28ae393c52e8
Deven Patel <deven.h.j.patel+softwaredev@gmail.com>
2023-08-03 09:45:38 -0400

    AM algo

--- Algorithm/w01d4/w1d4-decode-string.js
+++ Algorithm/w01d4/w1d4-decode-string.js
@@ -17,4 +17,43 @@ const expected2 = "aaabbccccccccccccdddddddddd";
  *    after indicating how many times the character occurs.
  * @returns {string} The given str decoded / expanded.
  */
-function decodeStr(str) {}
+function decodeStr(str) {
+  let decodedString = "";
+
+  for (let i = 0; i < str.length; i++) {
+    const char = str[i];
+
+    // If the current character is a letter, check if there is an integer after it.
+    if (/[a-zA-Z]/.test(char)) {
+      let numString = "";
+      i++;
+
+      // Extract the integer following the current character, if any.
+      while (i < str.length && /[0-9]/.test(str[i])) {
+        numString += str[i];
+        i++;
+      }
+
+      // Convert the extracted integer string to an actual integer.
+      const repeatCount = parseInt(numString, 10) || 1;
+
+      // Repeat the current character by the repeatCount and add it to the decoded string.
+      decodedString += char.repeat(repeatCount);
+
+      // Decrement i by 1 so that the loop increments it back to the correct position.
+      i--;
+    }
+  }
+
+  return decodedString;
+}
+
+// Test cases
+
+console.log(decodeStr(str1)); 
+// Output: "aaabbcddd"
+
+
+console.log(decodeStr(str2)); 
+// Output: "aaabbccccccccccccdddddddddd"
+
--- Algorithm/w01d4/w1d4-encode-string.js
+++ Algorithm/w01d4/w1d4-encode-string.js
@@ -1,16 +1,4 @@
-/* 
-  Given in an alumni interview in 2021.
 
-  String Encode
-
-  You are given a string that may contain sequences of consecutive characters.
-  Create a function to shorten a string by including the character,
-  then the number of times it appears. 
-  
-  
-  If final result is not shorter (such as "bb" => "b2" ),
-  return the original string.
-  */
 
 const str1 = "aaaabbcddd";
 const expected1 = "a4b2c1d3";
@@ -33,4 +21,47 @@ const expected4 = "bbcc";
  * @param {string} str The string to encode.
  * @returns {string} The given string encoded.
  */
-function encodeStr(str) {}
+
+function encodeStr(str) {
+  // If the input string is empty or null, return an empty string.
+  if (!str) {
+    return "";
+  }
+
+  // Initialize variables to keep track of the encoded string.
+  let encodedString = "";
+  let currentChar = str[0];
+  let count = 1;
+
+  // Iterate through the characters of the input string starting from the second character (index 1).
+  for (let i = 1; i < str.length; i++) {
+    // If the current character is the same as the previous character, increment the count.
+    if (str[i] === currentChar) {
+      count++;
+    } else { // If a new character is encountered:
+      // Append the previous character and its count to the encoded string.
+      encodedString += currentChar + count;
+      // Update the current character and reset the count to 1 for the new character.
+      currentChar = str[i];
+      count = 1;
+    }
+  }
+
+  // After the loop, add the last character and its count to the encoded string.
+  encodedString += currentChar + count;
+
+  // Check if the encoded string is shorter than the original string.
+  // If it is, return the encoded string; otherwise, return the original string.
+  return encodedString.length < str.length ? encodedString : str;
+}
+
+
+  // Test cases
+
+  console.log(encodeStr(str1)); // Output: "a4b2c1d3"
+
+  console.log(encodeStr(str2)); // Output: ""
+  
+  console.log(encodeStr(str3)); // Output: "a"
+  
+  console.log(encodeStr(str4)); // Output: "bbcc"
